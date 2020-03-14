0da18b8b3fb15f74f5b0be551558a4d0242f19c0
Alejandro Avilés <ome@bonfire.link>
2020-03-14 19:53:44 +0100

    Add script to publish to gh-pages branch

---
+++ bin/publish.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Source: https://gohugo.io/hosting-and-deployment/hosting-on-github/#put-it-into-a-script-1
+
+set -e
+
+if [ "$(git status -s)" ]; then
+  echo "The working directory is dirty. Please commit any pending changes."
+  exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf dist
+mkdir dist
+git worktree prune
+rm -rf .git/worktrees/dist/
+
+echo "Checking out gh-pages branch into dist"
+git worktree add -B gh-pages dist origin/gh-pages
+
+echo "Removing existing files"
+rm -rf dist/*
+
+echo "Generating site"
+npm run build
+
+echo "Updating gh-pages branch"
+cd dist && git add --all && git commit -m "Publish to gh-pages (publish.sh)"
+
+echo "Pushing to github"
+git push
