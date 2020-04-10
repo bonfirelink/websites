573d0bddf3101b4c17b56fa1d33b42d2e6e322b7
Alejandro Avilés <ome@bonfire.link>
2020-04-10 15:48:41 +0200

    Extract common site files
    This allows to have multiple Hugo sites share style, fonts, etc. by
    symlinking files in the sites/common subdirectory.

--- sites/landing/postcss.config.js
+++ sites/landing/postcss.config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      browsers: "last 2 versions"
-    },
-    autoprefixer: {}
-  }
-};
+../.common/postcss.config.js
