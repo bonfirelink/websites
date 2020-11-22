f609c74a4d50b66864a9032b193028d73cd17cc5
Alejandro Avilés <ome@bonfire.link>
2020-11-22 17:04:18 +0100

    Improve notadao redirection timer

--- sites/dao/src/index.js
+++ sites/dao/src/index.js
@@ -4,11 +4,13 @@ import "./scss/main.scss";
 import * as common from "./common.js";
 
 function redirectToAragon() {
+  const redirect = document.getElementById("redirect");
   const progress = document.getElementById("progress");
-  progress.style.transition = 'width 10s linear';
+  const transitionDuration = getComputedStyle(progress).transitionDuration;
+  const milliseconds = parseInt(transitionDuration.replace('s', '')) * 1000;
   progress.style.width = '0%';
   // TODO: add debug flag to disable redirection
-  setTimeout((() => window.location.href = 'https://mainnet.aragon.org/#/notadao'), 10000);
+  setTimeout((() => window.location.href = redirect.href), milliseconds);
 }
 
 common.ready(redirectToAragon);
